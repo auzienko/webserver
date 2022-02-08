766e6b911855462aa3e346a14f1374b2cff4ab83
Aleksandr Uzienko <a.uzienko@gmail.com>
2022-02-08 12:12:24 +0300

    fix request struct names

--- includes/structs.hpp
+++ includes/structs.hpp
@@ -6,7 +6,7 @@
 /*   By: zcris <zcris@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 11:29:26 by zcris             #+#    #+#             */
-/*   Updated: 2022/02/08 12:02:45 by zcris            ###   ########.fr       */
+/*   Updated: 2022/02/08 12:11:50 by zcris            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,30 +36,30 @@ typedef struct tag_server {
 
 typedef struct tag_requestHeader {
     std::string Method;
-    std::string Request-URI;
-    std::string HTTP-Version;
+    std::string Request_URI;
+    std::string HTTP_Version;
     std::string Host;
     std::string Connection;
-    int Content-Length;
+    int Content_Length;
 
-    std::string Cache-Control;
-    std::string Accept-Encoding;
-    std::string Accept-Language;
-    std::string User-Agent;
-    int Upgrade-Insecure-Requests;
+    std::string Cache_Control;
+    std::string Accept_Encoding;
+    std::string Accept_Language;
+    std::string User_Agent;
+    int Upgrade_Insecure_Requests;
     
     std::string Accept;
-    std::string Accept-Charset;
+    std::string Accept_Charset;
     std::string Authorization;
     std::string Expect;
     std::string From;
-    std::string If-Match;
-    std::string If-Modified-Since;
-    std::string If-None-Match;
-    std::string If-Range;
-    std::string If-Unmodified-Since;
-    std::string Max-Forwards;
-    std::string Proxy-Authorization;
+    std::string If_Match;
+    std::string If_Modified_Since;
+    std::string If_None_Match;
+    std::string If_Range;
+    std::string If_Unmodified_Since;
+    std::string Max_Forwards;
+    std::string Proxy_Authorization;
     std::string Range;
     std::string Referer;
     std::string TE;
