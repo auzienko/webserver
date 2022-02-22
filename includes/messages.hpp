892e00f2134489e9f9c56052d8dc7108d718f38d
Wholemeal Gaunt <wgaunt@mi-u5.msk.21-school.ru>
2022-02-22 19:42:39 +0300

    Typo

--- includes/messages.hpp
+++ includes/messages.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   messages.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zcris <zcris@student.21-school.ru>         +#+  +:+       +#+        */
+/*   By: wgaunt <wgaunt@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 18:11:09 by zcris             #+#    #+#             */
-/*   Updated: 2022/02/07 18:27:09 by zcris            ###   ########.fr       */
+/*   Updated: 2022/02/22 19:33:07 by wgaunt           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 #define MESSAGE_SUCCESS "✔️ SUCCESS!"
 #define MESSAGE_FAIL "❌ FAIL!"
-#define MESSAGE_SERVER_STARTED "🟢 Server starded"
+#define MESSAGE_SERVER_STARTED "🟢 Server started"
 #define MESSAGE_TRY_SEND_DATA "Server: sending data to client..."
 #define MESSAGE_TRY_START_SERV "🟡 Try to start webserver..."
 #define MESSAGE_CREATE_LISTEN_SOCKETS "~~ Create server's listen sockets..."
