c108df9a09b133e00d096d8d08123fc94f80db6d
Aleksandr Uzienko <a.uzienko@gmail.com>
2022-04-13 11:19:33 +0300

    test cgi in browser without parser

--- srcs/classes/Tasks/CgiOutputTask.cpp
+++ srcs/classes/Tasks/CgiOutputTask.cpp
@@ -1,8 +1,9 @@
 #include "../../../includes/classes/Tasks/CgiOutputTask.hpp"
+
 #include "../../../includes/classes/MimeTypes.hpp"
 
 CgiOutputTask::CgiOutputTask(AConnection* connection, int const& fd,
-                           int const& parentFd)
+                             int const& parentFd)
     : ATask(LOCAL_CGI_OUTPUT, fd),
       _connection(connection),
       _parentFd(parentFd) {}
@@ -10,16 +11,28 @@ CgiOutputTask::CgiOutputTask(AConnection* connection, int const& fd,
 CgiOutputTask::~CgiOutputTask() {}
 
 int CgiOutputTask::collectData(void) {
-  std::cout << "~~~ CgiOutputTask::collectData:\n" << _connection->getInputData().str() << std::endl;
-  setStatus(DONE);
+  setStatus(READY_TO_HANDLE);
+  executeTask();
   return 0;
 }
 
 int CgiOutputTask::executeTask(void) {
-    std::cout << "~~~ CgiOutputTask::executeTask\n";
-     return 0; }
+  setStatus(EXECUTION);
+  std::cout << "~~~ CgiOutputTask::executeTask\n";
+
+  std::stringstream result;
+  result << "HTTP/1.1 200 OK\r\n";
+  result << "Connection: keep-alive\r\n";
+  result << _connection->getInputData().str();
+
+  _connection->getConnectionManager()->at(_parentFd)->addToOutput(result.str());
+  _connection->getConnectionManager()->at(_parentFd)->getTask()->setStatus(
+      READY_TO_SEND);
+  setStatus(DONE);
+  return 0;
+}
 
 int CgiOutputTask::sendResult(void) {
-    std::cout << "~~~ CgiOutputTask::sendResult\n";
+  std::cout << "~~~ CgiOutputTask::sendResult\n";
   return 0;
 }
