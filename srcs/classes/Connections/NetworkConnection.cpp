920e94003ad2164dc9a9a0fb36265b373df562db
Aleksandr Uzienko <a.uzienko@gmail.com>
2022-05-09 19:08:38 +0300

    some network task refactor

--- srcs/classes/Connections/NetworkConnection.cpp
+++ srcs/classes/Connections/NetworkConnection.cpp
@@ -11,12 +11,41 @@ NetworkConnection::NetworkConnection(
 NetworkConnection::~NetworkConnection() {}
 
 int NetworkConnection::hasDataToReadEvent(void) {
-  if (!_task) return 0;
-  if (_task->getStatus() > READY_TO_HANDLE) {
-    readyToAcceptDataEvent();
-    return 0;
+  if (_task) _io();
+  return 0;
+}
+
+int NetworkConnection::readyToAcceptDataEvent(void) {
+  if (_task) _io();
+  return 0;
+}
+
+void NetworkConnection::_io() {
+  int taskStatus = _task->getStatus();
+  switch (taskStatus) {
+    case NEW:
+      _task->setStatus(READING);
+    case READING:
+      _reading();
+      break;
+    case READY_TO_HANDLE:
+    case EXECUTION:
+      _task->doTask();
+      break;
+    case READY_TO_SEND:
+    case SENDING:
+      _writing();
+      break;
+    case DONE:
+      getConnectionManager()->remove(_idFd);
+      break;
+    default:
+      break;
   }
-  _task->setStatus(READING);
+}
+
+int NetworkConnection::_reading(void) {
+  setLastActivity();
   int nbytes;
   char buf[DEFAULT_BUFLEN];
   memset(buf, 0, DEFAULT_BUFLEN);
@@ -29,47 +58,35 @@ int NetworkConnection::hasDataToReadEvent(void) {
     std::cout << "fd (NetworkConnection): " << _idFd << " reading no data\n";
     _task->doTask();
     _input.str(std::string());
-
     return 0;
   } else {
     _input << buf;
-    std::cout << "\n⬇ ⬇ ⬇ fd (NetworkConnection): " << _idFd << " READ " << nbytes / 1024. << "Kb data\n";
-    setLastActivity();
+    std::cout << "\n⬇ ⬇ ⬇ fd (NetworkConnection): " << _idFd << " READ "
+              << nbytes / 1024. << "Kb data\n";
     _task->doTask();
     _input.str(std::string());
   }
   return 0;
 }
 
-int NetworkConnection::readyToAcceptDataEvent(void) {
-  if (_task->getStatus() >= READY_TO_HANDLE && _task->getStatus() < SENDING) {
-    setLastActivity();
-    _task->doTask();
-    return 0;
-  }
-  if (!_task || _task->getStatus() == NEW) return 0;
+int NetworkConnection::_writing(void) {
+  setLastActivity();
   int ret = 0;
   int nbytes = 0;
-  if (_task->getStatus() == SENDING) {
-    if (static_cast<std::string::size_type>(_wrote) < _output.str().length()) {
-      char buf[DEFAULT_BUFLEN];
-      _output.get(buf, DEFAULT_BUFLEN, 0);
-      nbytes = send(_idFd, buf, strlen(buf), 0);
-      if (nbytes) std::cout << "\n⬆ ⬆ ⬆ fd (NetworkConnection): " << _idFd << " WROTE "
-                << nbytes /* / 1024. */ << "Kb data result code: " << ret << std::endl;
-      _wrote += nbytes;
-      setLastActivity();
-      return 0;
-    }
-
-    _task->setStatus(DONE);
-    if (_output.str().length() < 100)
-      std::cout << _output.str() << std::endl;
-    std::cout << _wrote << " bytes wrote totaly of " << _output.str().length() << std::endl;
-    /* Временный фикс (работает как close тип соединения)
-    if (!_task->getIsKeepAlive())*/ getConnectionManager()->remove(_idFd);
-    //остался еще фд. не забудь про аутпут
-
+  if (static_cast<std::string::size_type>(_wrote) < _output.str().length()) {
+    char buf[DEFAULT_BUFLEN];
+    _output.get(buf, DEFAULT_BUFLEN, 0);
+    nbytes = send(_idFd, buf, strlen(buf), 0);
+    if (nbytes)
+      std::cout << "\n⬆ ⬆ ⬆ fd (NetworkConnection): " << _idFd << " WROTE "
+                << nbytes /* / 1024. */ << "B data result code: " << ret
+                << std::endl;
+    _wrote += nbytes;
+    return 0;
   }
+  _task->setStatus(DONE);
+  if (_output.str().length() < 100) std::cout << _output.str() << std::endl;
+  std::cout << _wrote << " bytes wrote totaly of " << _output.str().length()
+            << std::endl;
   return ret;
 }
